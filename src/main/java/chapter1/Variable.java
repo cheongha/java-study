1076eeba7d7535ddf624f7903b57238425b06768
cheongha <sch7188@gmail.com>
2023-08-05 16:53:41 +0900

    변수와 자료형

---
+++ src/main/java/chapter1/Variable.java
@@ -0,0 +1,24 @@
+package chapter1;
+
+public class Variable {
+
+    public static void main (String[] args){
+
+        // 변수 선언
+        int num;
+        // 변수 값 할당
+        num = 10;
+
+        System.out.println(num);
+
+        // 변수 선언과 동시에 0으로 초기화
+        // int니까 4바이트로 저장
+        int level = 0;
+        System.out.println(level);
+
+
+        final float PI = 3.14F;
+
+
+    }
+}
